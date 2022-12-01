374f651642cba9ce8729734b11fa66d017a5b0a8
Dennis Riis <dennis.riis@schibsted.com>
2022-12-01 20:30:26 +0100

    Day 1 part 1

---
+++ day1/Program.cs
@@ -0,0 +1,20 @@
+﻿IEnumerable<int> Elves(string[] lines)
+{
+    int sum = 0;
+    foreach (string line in lines)
+    {
+        if (line == "")
+        {
+            yield return sum;
+            sum = 0;
+        }
+
+        if (int.TryParse(line, out int x))
+        {
+            sum += x;
+        }
+    }
+}
+var lines = await File.ReadAllLinesAsync("input.txt");
+int largest = Elves(lines).Max();
+Console.WriteLine(largest);
