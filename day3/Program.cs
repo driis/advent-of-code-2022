cf0467b2fbcf38b13c3edf5409f958944503bd4a
Dennis Riis <dr@driis.dk>
2022-12-04 11:41:32 +0100

    days 3 and 4

---
+++ day3/Program.cs
@@ -0,0 +1,24 @@
+﻿int Priority(char ch) => ch switch
+{
+    < 'a' => (ch - 'A') + 27 , 
+    _ => ch - 'a' + 1
+};
+
+var lines = await File.ReadAllLinesAsync("input.txt");
+var rucksacks = lines.Select(line =>
+{
+    var div = line.Length / 2;
+    return (part1: line[..div], part2: line[div..]);
+}).ToArray();
+
+var bothCompartments = rucksacks.Select(r => r.part1.Intersect(r.part2));
+var prioritySum = bothCompartments.SelectMany(x => x).Sum(Priority);
+Console.WriteLine($"Sum of priorities: {prioritySum}");
+
+// Part 2
+var groups = lines.Select((line,idx) => new {line,g = idx /3 }).GroupBy(x => x.g).Select(g => g.ToArray());
+var badges = groups.Select(group => group[0].line.Intersect(group[1].line).Intersect(group[2].line)).SelectMany(x => x);
+var badgeSum = badges.Sum(Priority);
+Console.WriteLine($"Sum of badges: {badgeSum}");
+
+    
---
+++ day4/Program.cs
@@ -0,0 +1,32 @@
+﻿Range[] RangesFromLine(string line)
+{
+    var parts = line.Split(',');
+    var ranges = parts.Select(p =>
+    {
+        var n = p.Split('-');
+        var begin = int.Parse(n[0]);
+        var end = int.Parse(n[1]);
+        return begin..end;
+    }).ToArray();
+    return ranges;
+}
+
+bool IsContained(Range first, Range second)
+{
+    bool IsIn(Range f, Range s) => f.Start.Value >= s.Start.Value && f.End.Value <= s.End.Value;
+    return IsIn(first, second) || IsIn(second, first);
+}
+
+bool IsPartiallyContained(Range first, Range second)
+{
+    bool IsIn(Range f, Range s) => (f.Start.Value >= s.Start.Value && f.Start.Value <= s.End.Value);
+    return IsIn(first, second) || IsIn(second, first);
+}
+var lines = await File.ReadAllLinesAsync("input.txt");
+var ranges = lines.Select(RangesFromLine).ToArray();
+int countContained = ranges.Count(x=>IsContained(x[0], x[1]));
+Console.WriteLine($"Fully contained ranges:\t\t{countContained}");
+
+int countPartiallyContained = ranges.Count(x => IsPartiallyContained(x[0], x[1]));
+Console.WriteLine($"Partially contained ranges:\t{countPartiallyContained}");
+
