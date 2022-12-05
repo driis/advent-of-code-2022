ece84aed810dc9b0a2541082416fa96bc80ba955
Dennis Riis <dennis.riis@schibsted.com>
2022-12-05 21:56:21 +0100

    solve day5

--- day5/Program.cs
+++ day5/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Stack<char>[] ParseStacks(string[] input)
+{
+    var indices = new[] {1, 5, 9, 13, 17, 21, 25, 29, 33};
+    var stacks = indices.Select(i => new Stack<char>()).ToArray();
+    foreach (string line in input.Reverse())
+    {
+        for (int i = 0; i < indices.Length; i++)
+        {
+            char x = line[indices[i]];
+            if (x != ' ')
+            {
+                stacks[i].Push(x);
+            }
+        }
+    }
+
+    return stacks;
+}
+
+void Move<T>(int count, Stack<T> from, Stack<T> to)
+{
+    for (int i = 0; i < count ; i++)
+    {
+        var item = from.Pop();
+        to.Push(item);
+    }
+}
+
+
+void MoveInBlocks<T>(int count, Stack<T> from, Stack<T> to)
+{
+    var temp = new T[count];
+    for (int i = 0; i < count ; i++)
+    {
+        temp[i] = from.Pop();
+    }
+
+    foreach (var item in temp.Reverse())
+    {
+        to.Push(item);
+    }
+}
+
+var lines = await File.ReadAllLinesAsync("input.txt");
+var state = lines.TakeWhile(x => x.StartsWith("[")).ToArray();
+var moves = lines.SkipWhile(x => x != "").Skip(1).ToArray();
+
+var stacks = ParseStacks(state);
+foreach (string move in moves)
+{
+    var parts = move.Split(' ');
+    int amt = Int32.Parse(parts[1]);
+    int from = Int32.Parse(parts[3]) - 1;
+    int to = Int32.Parse(parts[5]) - 1;
+    MoveInBlocks(amt, stacks[from], stacks[to]);
+}
+var msg = new string(stacks.Select(s => s.Pop()).ToArray());
+WriteLine(msg);
