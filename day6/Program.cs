af9a7d71ebfe195e2b1a3c87a906fc3d17bce012
Dennis Riis <dennis.riis@schibsted.com>
2022-12-07 20:20:02 +0100

    day6

---
+++ day6/Program.cs
@@ -0,0 +1,25 @@
+﻿bool AllIsDifferent(char[] data)
+{
+    for (int i = 0; i < data.Length - 1; i++)
+    {
+        int offset = i + 1;
+        int cur = data[i];
+        if (data[offset..].Any(x => x == cur))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+const int msgSize = 14;
+string data = File.ReadAllText("input.txt");
+int i = 0;
+var buf = data[..msgSize].ToArray();
+for (i = msgSize - 1; i < data.Length; i++)
+{
+    buf[i % msgSize] = data[i];
+    if (AllIsDifferent(buf))
+        break;
+}
+Console.WriteLine(i+1);
