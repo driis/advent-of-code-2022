cd9730c7b3f47997d188ecda5469444868b2138f
Dennis Riis <dr@driis.dk>
2022-12-11 18:14:21 +0100

    day 9 part 2

--- day9/Program.cs
+++ day9/Program.cs
@@ -19,31 +19,43 @@ Move ParseMove(string line)
     return new Move(dir, dist);
 }
 
-var head = new Pos(0, 0);
-var tail = new Pos(0, 0);
-var moves = input.Select(ParseMove);
-HashSet<Pos> positions = new() {tail};
+Pos DoMove(Direction dir, Pos pos) => dir switch
+{
+    Direction.Right => pos with {X = pos.X + 1},
+    Direction.Left => pos with {X = pos.X - 1},
+    Direction.Down => pos with {Y = pos.Y + 1},
+    Direction.Up => pos with {Y = pos.Y - 1},
+    _ => pos
+};
 
+const int SnakeLength = 10; // 2 for Part 1, 10 for part 2
+var snake = Enumerable.Repeat(new Pos(0, 0), SnakeLength).ToArray();
+var moves = input.Select(ParseMove);
+HashSet<Pos> positions = new() {snake[SnakeLength - 1]};
 
 foreach (Move m in moves)
 {
     for (int i = 0; i < m.Distance; i++)
     {
-        var prev = head;
-        head = m.Direction switch
-        {
-            Direction.Right => head with {X = head.X + 1},
-            Direction.Left => head with {X = head.X - 1},
-            Direction.Down => head with {Y = head.Y + 1},
-            Direction.Up => head with {Y = head.Y - 1},
-            _ => head
-        };
-        if (Math.Abs(tail.X - head.X) > 1 || Math.Abs(tail.Y - head.Y) > 1)
-        {
-            tail = prev;
-            if (!positions.Contains(tail))
-                positions.Add(tail);
+        snake[0] = DoMove(m.Direction, snake[0]);
+        for (int z = 1; z < snake.Length; z++)
+        {            
+            var head = snake[z-1];
+            var tail = snake[z];
+            var deltaX = head.X - tail.X;
+            var deltaY = head.Y - tail.Y;
+            if (Math.Abs(deltaX) > 1 || Math.Abs(deltaY) > 1)
+            {
+                deltaX = deltaX < -1 ? -1 : deltaX > 1 ? 1 : deltaX;
+                deltaY = deltaY < -1 ? -1 : deltaY > 1 ? 1 : deltaY;
+                tail = tail with { X = tail.X + deltaX, Y = tail.Y +deltaY};
+                snake[z] = tail;
+            }                        
         }
+        
+        var last = snake[snake.Length-1];
+        if (!positions.Contains(last))
+            positions.Add(last);
     }
 }
 
