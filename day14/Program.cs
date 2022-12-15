32e500f94201596286d43f3ff8b862251b4bc5c2
Dennis Riis <dennis.riis@schibsted.com>
2022-12-15 17:11:20 +0100

    day14 parse

--- day14/Program.cs
+++ day14/Program.cs
@@ -1,2 +1,22 @@
-﻿x// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var input = await File.ReadAllLinesAsync(args.FirstOrDefault() ?? "input.txt");
+
+IEnumerable<Line> LineFromText(string line)
+{
+    var parts = line.Split(" -> ");
+    var coords = parts.Select(p =>
+    {
+        int[] data = p.Split(",").Select(z => Int32.Parse(z)).ToArray();
+        return new Coord(data[0], data[1]);
+    }).ToArray();
+
+    for (int i = 0; i < coords.Length - 1; i++)
+    {
+        yield return new Line(coords[i], coords[i + 1]);
+    }
+}
+
+var allLines = input.SelectMany(LineFromText).ToArray();
+Console.WriteLine($"Parsed {allLines.Length} lines.");
+
+record Coord(int X, int Y);
+record Line(Coord Begin, Coord End);
