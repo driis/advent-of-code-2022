b1f45ccf8f708fd261164539214f5629da2ba639
Dennis Riis <dr@driis.dk>
2022-12-21 21:23:08 +0100

    Solve day17 part 2

--- day17/Program.cs
+++ day17/Program.cs
@@ -1,7 +1,8 @@
 ﻿string input = File.ReadAllText(args.FirstOrDefault() ?? "input.txt");
 var movement = new Movement(input.Trim());
-
-var world = new char[7,12000];
+const int WorldWidth = 7;
+const int WorldHeight = 1000;
+var world = new char[WorldWidth,WorldHeight];
 var shapes = new Shape[] {
     new (new []{"####"}),
     new (new []{".#.",
@@ -20,6 +21,7 @@ var shapes = new Shape[] {
 
 long count = Int64.TryParse(args.Skip(1).FirstOrDefault(), out long c) ? c : 2022;
 long topIndex = 0;
+long topOffset = 0;
 for(long block = 0 ; block < count ; block++)
 {
     var shape = shapes[block%5] with { Y = topIndex + 4 };
@@ -31,18 +33,38 @@ for(long block = 0 ; block < count ; block++)
             '>' => shape.Right(world),
             _ => throw new ApplicationException($"Don't know move '{move}'")
         };        
+        if (movement.Index % movement.Input.Length == 0)
+        {
+            Console.WriteLine($"Exhausted input after {block} blocks - height is {topIndex + topOffset}");
+        }
     }   
     var newTop = shape.Y + shape.Height;
     topIndex = newTop > topIndex ? newTop : topIndex;
+    if (topIndex > WorldHeight - 20)
+    {
+        const int retainTop = 100;
+        var newWorld = new char[WorldWidth, WorldHeight];
+        var from = topIndex - retainTop;
+        for(long y = 0 ; y < retainTop ; y++)
+        {
+            for(int x = 0 ; x < WorldWidth ; x++)
+            {
+                newWorld[x,y] = world[x, from + y];
+            }
+        }
+        topOffset += topIndex - retainTop;
+        topIndex = retainTop;
+        world = newWorld;
+    }
 }
 
-Console.WriteLine(topIndex);
+Console.WriteLine(topIndex + topOffset);
 if (topIndex < 50)
 {
     for(long y = topIndex ; y >= 0 ; y--)
     {
         Console.Write('|');
-        for(int x = 0 ; x < 7 ; x++)
+        for(int x = 0 ; x < WorldWidth ; x++)
         {
             char ch = world[x,y];
             ch = ch==0 ? '.' : ch;
@@ -52,6 +74,24 @@ if (topIndex < 50)
     }
     Console.WriteLine("+-------+");
 }
+
+// Part 2 - by observation - after exhausting the input after 1723 blocks,
+// Then the same pattern recurs each 1725 blocks. THese 1725 blocks is 2709 pix high.
+// Do some math 
+const long firstIterationBlocks= 1723;
+const long blocksToRecur = 1725;
+const long heightPerRecur = 2709;
+
+const long rocks = 1_000_000_000_000;
+const long rocksAfterFirst = rocks - firstIterationBlocks;
+const long iterations = rocksAfterFirst / blocksToRecur;
+const long blocksRan = iterations * blocksToRecur + firstIterationBlocks;
+const long blocksRemaining = rocks - blocksRan;
+Console.WriteLine($"Part 2 blocks remaning {blocksRemaining} - run the program with input {blocksRemaining+ firstIterationBlocks}");
+// 5247 is the output of the program run with blocksRemaining + firstIterationBlocks as input
+const long remaining = 5247;
+const long height = heightPerRecur * iterations + remaining;
+Console.WriteLine($"Part 2 height: {height}");
 
 record Shape(string[] Lines)
 {
@@ -126,7 +166,7 @@ record Shape(string[] Lines)
 };
 record Movement(string Input)
 {
-    private int index = 0;
+    public int Index = 0;
 
-    public char NextMove() => Input[index++ % Input.Length];
+    public char NextMove() => Input[Index++ % Input.Length];
 }
