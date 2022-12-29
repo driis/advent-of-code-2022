ad2cd046e679dc8c6207620885a02f083097028c
Dennis Riis <dr@driis.dk>
2022-12-29 20:56:06 +0100

    day20 skeleton

---
+++ day20/Program.cs
@@ -0,0 +1,3 @@
+﻿var input = await File.ReadAllLinesAsync(args.FirstOrDefault() ?? "input.txt");
+var numbers = input.Select(x => Int32.Parse(x)).ToArray();
+Console.WriteLine($"Got {numbers.Length} numbers in input");
